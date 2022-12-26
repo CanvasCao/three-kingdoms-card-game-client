5d247ccdee4a3d381da28c3f2483863c666611d4
CanvasCao <1174478646@qq.com>
2022-12-27 10:51:14 +1100

    update attachFEInfoToCard

--- src/utils/cardUtils.ts
+++ src/utils/cardUtils.ts
@@ -20,49 +20,31 @@ export const attachFEInfoToCard = (card: Card) => {
         card.canPlayInMyTurn = false
     }
 
-    if (card.CN == BASIC_CARDS_CONFIG.SHA.CN ||
-        card.CN == BASIC_CARDS_CONFIG.LEI_SHA.CN ||
-        card.CN == BASIC_CARDS_CONFIG.HUO_SHA.CN ||
-        card.CN == BASIC_CARDS_CONFIG.TAO.CN ||
-        card.type == CARD_TYPE.EQUIPMENT ||
-        card.type == CARD_TYPE.SCROLL && card.CN !== SCROLL_CARDS_CONFIG.WU_XIE_KE_JI.CN
-    ) {
-        card.canPlayInMyTurn = true
-    } else {
-        card.canPlayInMyTurn = false
-    }
-
 
     // 暂时 只需要我的回合可以出的牌 因为我的响应阶段目前不需要指定目标
     // 1.可以多目标 2.前端无目标 因为以自己为目标 3.单目标 4.所有人都是目标(无需标记)
-
+    // 5.我的回合不能出的牌
     if (card.type == CARD_TYPE.EQUIPMENT) {
         card.targetMinMax = {min: 0, max: 0}
         card.noNeedSetTargetDueToImDefaultTarget = true
-        // 2.
+
     } else if ([
+        // 2.
         BASIC_CARDS_CONFIG.TAO.CN,
         SCROLL_CARDS_CONFIG.SHAN_DIAN.CN,
         SCROLL_CARDS_CONFIG.WU_ZHONG_SHENG_YOU.CN,
 
+        // 5.
         BASIC_CARDS_CONFIG.SHAN.CN,
         SCROLL_CARDS_CONFIG.WU_XIE_KE_JI.CN,
 
+        // 4.
         SCROLL_CARDS_CONFIG.NAN_MAN_RU_QIN.CN,
         SCROLL_CARDS_CONFIG.WAN_JIAN_QI_FA.CN,
         SCROLL_CARDS_CONFIG.TAO_YUAN_JIE_YI.CN,
         SCROLL_CARDS_CONFIG.WU_GU_FENG_DENG.CN,
     ].includes(card.CN)) {
         card.targetMinMax = {min: 0, max: 0}
-
-        // if ([
-        //     SCROLL_CARDS_CONFIG.NAN_MAN_RU_QIN.CN,
-        //     SCROLL_CARDS_CONFIG.WAN_JIAN_QI_FA.CN,
-        //     SCROLL_CARDS_CONFIG.TAO_YUAN_JIE_YI.CN,
-        //     SCROLL_CARDS_CONFIG.WU_GU_FENG_DENG.CN,
-        // ].includes(card.CN)) {
-        //     4.
-        // }
 
         // 2.
         if ([
@@ -100,11 +82,11 @@ export const attachFEInfoToCard = (card: Card) => {
             SCROLL_CARDS_CONFIG.SHUN_SHOU_QIAN_YANG.CN,
         ].includes(card.CN)) {
             card.couldHaveMultiTarget = true
-        } else {
+        }
+        // 3.
+        else {
             card.canOnlyHaveOneTarget = true
         }
-
-
     } else {
         throw new Error("有没有设置目标的牌" + card.CN)
     }
