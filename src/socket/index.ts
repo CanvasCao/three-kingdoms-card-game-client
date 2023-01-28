47ce59596ed2d18edb41f62469bd57597874c12e
CanvasCao <1174478646@qq.com>
2023-01-28 23:23:42 +1100

    add socket url

--- src/socket/index.ts
+++ src/socket/index.ts
@@ -1,8 +1,7 @@
 import {io} from './socket.io.esm.min.js'
 
-const socket = io('http://localhost:3000');
-// const socket = io('http://3.26.32.0:3000');
-
+const socket = io('http://3.24.228.1:3000');
+// const socket = io('http://localhost:3000');
 
 export {socket};
 
