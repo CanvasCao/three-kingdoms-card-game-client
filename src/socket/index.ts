1a00c95ddb763049298d01a6644ceb8a5124cf59
CanvasCao <1174478646@qq.com>
2023-09-23 10:11:39 +1000

    [SGS-152] show thinking hint when select a hero

--- src/socket/index.ts
+++ src/socket/index.ts
@@ -1,6 +1,6 @@
 import {io} from './socket.io.esm.min.js'
 
- // const socket = io('http://3.104.104.183:3000');
+ // const socket = io('http://3.25.140.70:3000');
 const socket = io('http://localhost:3000');
 
 export {socket};
